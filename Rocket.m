3fa1c38804654de9e27d537b0f37bed8723e412a
Antonio Hernández-Rodicio Mateos <antoniohernandez-rodiciomateos@MacBook-Pro-de-Antonio-2.local>
2023-10-09 22:29:51 +0200

    Remove icon file

--- Rocket.m
+++ Rocket.m
@@ -1,8 +1,9 @@
 classdef Rocket
     properties
-        isp = [310, 310, 330]
-        strcoeff = [0.10, 0.12, 0.14]
-        mstruct = zeros(3, 1)
-        
+
+        isp = [310, 310, 330]           % s
+        strcoeff = [0.10, 0.12, 0.14]   % %
+        mstruct = zeros(3, 1)           % kg
+
     end
 end
