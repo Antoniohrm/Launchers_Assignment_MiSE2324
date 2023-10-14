a90aae44e76141546f6f35a60c707915d0a06630
Antonio Hernández-Rodicio Mateos <antoniohernandez-rodiciomateos@MacBook-Pro-de-Antonio-2.local>
2023-10-14 20:22:59 +0200

    Advancing in EndoAtm Flight

--- endoAtmPhase.m
+++ endoAtmPhase.m
@@ -3,14 +3,13 @@ function [t, state, Rocket, Mission] = endoAtmPhase(Rocket, Mission)
 state0 = zeros(1, 7);
 state0(1:3) = Rocket.r(1, :);
 state0(4:6) = Rocket.v(1, :);
-%state0(4:6) = [1, 1, 1];
 state0(7) = Rocket.m0(Rocket.actstage);
 
-max_time = Rocket.tstage(1);
-curstate = [];
+inttime = Rocket.tstage;
 
-[t, state] = ode45(@(t, state) endoAtmDer(t, state, Rocket, Mission), [0, max_time], state0);
-%[t, ders] = endoAtmDer(0, state0, Rocket, Mission)
+[t, state] = ode45(@(t, state) endoAtmDer(t, state, Rocket, Mission), [0, inttime(Rocket.actstage)], state0);
+% [ders] = endoAtmDer(0, state0, Rocket, Mission)
 % size(ders)
+% t = 0;
 
 end
