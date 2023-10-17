f3eeaab83abb758c11acfb2f629ec43543f67425
Carlos22Quintana <145041995+Carlos22Quintana@users.noreply.github.com>
2023-10-17 20:33:30 +0200

    se me olvidala la funcion de eventos

---
+++ eventsfun.m
@@ -0,0 +1,28 @@
+function [check, stops, direction] = eventsfun(t, state, Rocket, Mission)
+
+h = norm(state(1:3)) - Mission.re;
+
+check(1) = h-100;
+stops(1) = 0; %
+direction(1) = 1;
+
+
+
+
+% h = norm(state(1:3)) - Mission.re;
+% 
+% if (norm(state(1:3)) - Mission.re) < Mission.vrlim
+%     %VR
+%     check(1) = h-100;
+%     stops(1) = 1; %
+%     direction(1) = 1;
+% else
+%     check(1) = h;
+%     stops(1) = 1;
+%     direction(1) = -1;
+% end
+% % mf1 = Rocket.m0(1) - Rocket.mprop(1);
+% % check(2) = state(7) - mf1;
+% % stops(2) = 1;
+% % direction(2) = -1;
+end
