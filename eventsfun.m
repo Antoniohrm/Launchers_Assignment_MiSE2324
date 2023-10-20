c848ee7904b3c58410918ad8d7cefcacfa948a4e
Carlos22Quintana <145041995+Carlos22Quintana@users.noreply.github.com>
2023-10-20 21:04:12 +0200

    Kick angle and second ode45

--- eventsfun.m
+++ eventsfun.m
@@ -1,16 +1,16 @@
 function [check, stops, direction] = eventsfun(t, state, Rocket, Mission)
 
 h = norm(state(1:3)) - Mission.re;
-
-check(1) = h-100;
-stops(1) = 1; %
-direction(1) = 1;
-
-% mf1 = Rocket.m0(1) - Rocket.mprop(1);
-% check(2) = state(7) - mf1;
-% stops(2) = 1;
-% direction(2) = -1;
-
+if h < 101 && Rocket.actstage == 1
+    check(1) = h-100;
+    stops(1) = 1; %
+    direction(1) = 1;
+else
+    mf1 = Rocket.m0(1) - Rocket.mprop(1);
+    check(2) = state(7) - mf1;
+    stops(2) = 1;
+    direction(2) = -1;
+end
 
 % h = norm(state(1:3)) - Mission.re;
 % 
