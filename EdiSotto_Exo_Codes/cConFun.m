0f199fea37774cb9ef8bba0fe25bc66d90150414
Carlos22Quintana <145041995+Carlos22Quintana@users.noreply.github.com>
2023-10-22 18:11:07 +0200

    comments exo

--- EdiSotto_Exo_Codes/cConFun.m
+++ EdiSotto_Exo_Codes/cConFun.m
@@ -3,23 +3,23 @@ function [c, ceq] = cConFun(x);
 %
 global state0 finalRa mu
 %
-propTime = x(1)*1e+2;
-pr       = x(2:4)';
-pv       = x(5:7)';
+propTime = x(1)*1e+2; %denormalize
+pr       = x(2:4)'; %Transpuesto
+pv       = x(5:7)'; %Transpuesto
 %
 augState = [state0, pr, pv];%
-[tExo  solExo] = ode45(@exoFlightDeriv,[0 propTime], augState);
+[tExo  solExo] = ode45(@exoFlightDeriv,[0 propTime], augState); %propagate exo flight
 %
-Xf = solExo(end,1:6);
+Xf = solExo(end,1:6); %Valores finales (cartesianos)
 %
-eleF = AST_pvecle(Xf,mu);
+eleF = AST_pvecle(Xf,mu); %valores finales (elementos orbitales)
 %
 SAXI_F = eleF(1);
 ECC_F  = eleF(2);
 %
-raf    = SAXI_F*(1 + ECC_F);
+raf    = SAXI_F*(1 + ECC_F); %radio apoapsis final
 %
-ceq   = (raf - finalRa)*1e-7;
+ceq   = (raf - finalRa)*1e-7; % Coeficiente de error, cost function
 c = [];
 
 
--- EdiSotto_Exo_Codes/mainOptCont.m
+++ EdiSotto_Exo_Codes/mainOptCont.m
@@ -11,28 +11,28 @@ global state0  mu finalRa
 mu = 398602*1e+9;
 Re = 6378140;
 wV = [ 0 0  2*pi/86164];
-rp = 163*1e+3 +Re;
+rp = 163*1e+3 +Re; %radio perigeo
 %
 vp = 8260 + 260;% This values are obtained through the video of the launch VA244. The velocity in the video (8260 m/s) 
                 % is relative velocity modulus. The added value of 260 m/s
                 % is the projection of the Earth Velocity at Kourou on the
                 % launch Azimuth direction (56 deg)
 %
-RAAN = 0;
-INCL = 56*pi/180;
-TRAN = 0*pi/180;
-WPER = 20*pi/180;
+RAAN = 0;           %[rad]
+INCL = 56*pi/180;   %[rad]
+TRAN = 0*pi/180;    %[rad]
+WPER = 20*pi/180;   %[rad]
 %
 %
-E    = vp^2/2 - mu/rp;
-SAXI = -mu/(2*E);
+E    = vp^2/2 - mu/rp; % Orbit Energy
+SAXI = -mu/(2*E); % Semimajor Axis
 %
-ECC  = 1 - rp/SAXI;
-ra   = SAXI*(1 + ECC);
+ECC  = 1 - rp/SAXI; % e=(1-r_peri/a)
+ra   = SAXI*(1 + ECC); %r_apo=a*(1 + e)
 %
-ele0 = [ SAXI, ECC, INCL, RAAN, WPER, TRAN];
+ele0 = [ SAXI, ECC, INCL, RAAN, WPER, TRAN]; % elementos orbitales finales
 %
-X    = AST_pelvec(ele0, mu);
+X    = AST_pelvec(ele0, mu); % conversion de elementos orbitales a cartesianas
 %
 % Initialise the primer assuming directed along the velocity vector and the
 % angualar velocity as the orbit mean motion 
@@ -40,33 +40,35 @@ X    = AST_pelvec(ele0, mu);
 r0 = X(1:3);
 v0 = X(4:6);
 %
-velIne = norm(v0)
+velIne = norm(v0) %velocidad inercial
 %
-velRel = v0 - cross(wV, r0);
+velRel = v0 - cross(wV, r0); %velocidad relativa a la superficie (Kourou)
 %
-h0   = cross(r0,v0);
+h0   = cross(r0,v0); % momento angular
 
-wOrb = h0/rp^2; 
+wOrb = h0/rp^2;  % velocidad angular ¿ en el perigeo ?
 %
 %pvU = X(4:6)/norm(X(4:6));
-pvU = velRel/norm(velRel);
+pvU = velRel/norm(velRel); %lagrarian multipliers in the orbit.
 prU = -cross(wOrb, pvU);% the minus sign comes from the primer dynamics prU = -pVUDot 
 %
 augState0 = [X, prU, pvU];
 %
-propTime0 = 200;%600;%639; %sec
-finalRa = 22922*1e+3 + Re;
+propTime0 = 639; %200;%600;%639; %sec propellant time [sec]
+finalRa = 22922*1e+3 + Re; %final apoapsis radius [m]
 %finalRp = 3.9574e+05;
 %
-opt0Sc     = [propTime0*1e-2, prU, pvU];
+opt0Sc     = [propTime0*1e-2, prU, pvU]; %optimization initializing values
 %
 state0 = X;
 %
-xBound(1, 1:2) = [200 1000]*1e-2;
+%Time optimization bounds
+xBound(1, 1:2) = [200 1000]*1e-2; % normalized
+% Thrust angular velocity (pru) optimization bounds
 xBound(2, 1:2) = [-1 1]*30*pi/180; % Thrust angular velocity pr is limited at 30 deg/s
 xBound(3, 1:2) = [-1 1]*30*pi/180;
 xBound(4, 1:2) = [-1 1]*30*pi/180;
-%
+% Thrust direction (pvu) optimization bounds
 xBound(5, 1:2) = [-1 1];
 xBound(6, 1:2) = [-1 1];
 xBound(7, 1:2) = [-1 1];
@@ -84,24 +86,24 @@ nonlcon = @cConFun;
 options = optimoptions('fmincon','Display','iter', 'Algorithm', 'sqp','MaxFunctionEvaluations',3000);
 Xsc = fmincon(@fSolveFun,opt0Sc',[],[],[],[],[xlb],[xub],nonlcon, options);
 %
-propTime = Xsc(1)*1e+2
+propTime = Xsc(1)*1e+2 %valor optimo 1 (propellant time) denormalizado
 %
 augState = [state0, Xsc(2:4)', Xsc(5:7)'];
 
-[tExo  solExo] = ode45(@exoFlightDeriv,[0 propTime], augState);
+[tExo  solExo] = ode45(@exoFlightDeriv,[0 propTime], augState); % determinacion del augmented state para la propagación final.
 %
-pos = solExo(:, 1:3);
-vel = solExo(:, 4:6);
+pos = solExo(:, 1:3); % posicion durante todo el trayecto
+vel = solExo(:, 4:6); % velocity durante todo el trayecto
 %
 for ii =1:length(tExo)
-    posN(ii) = norm(pos(ii,:));
-    velN(ii) = norm(vel(ii,:));
+    posN(ii) = norm(pos(ii,:)); % Modulo de la posicion en el trayecto (intento de radio)
+    velN(ii) = norm(vel(ii,:)); % Modulo de la velocidad en el trayecto 
     
     velRN(ii) = norm(vel(ii,:) - cross(wV, pos(ii,:)));
 end
 %
 figure
-plot(tExo, (posN - Re)*1e-3,'r', 'LineWidth',3)
+plot(tExo, (posN - Re)*1e-3,'r', 'LineWidth',3) %plot radio-radio de la tierra
 set(gca, 'FontSize',14)
 xlabel('Time [s]', 'FontSize',14)
 ylabel('Altitude [km]', 'FontSize',14)
@@ -109,35 +111,35 @@ grid on
 %
 %
 figure
-plot(tExo, (velN)*1e-3,'r','LineWidth',3);
+plot(tExo, (velN)*1e-3,'r','LineWidth',3); %plot velocidad
 set(gca, 'FontSize',14);
 xlabel('Time [s]', 'FontSize',14);
 ylabel('Inertal Velocity [km/s]', 'FontSize',14);
 grid on 
 %
 figure
-plot(tExo, (velRN)*1e-3,'r','LineWidth',3);
+plot(tExo, (velRN)*1e-3,'r','LineWidth',3); %plot velocidad relativa
 set(gca, 'FontSize',14);
 xlabel('Time [s]', 'FontSize',14);
 ylabel('Relative Velocity [km/s]', 'FontSize',14);
 grid on 
 %
 %exoFlightDeriv(0, augState0)
-Xf = solExo(end,1:6);
+Xf = solExo(end,1:6); %valor final de la orbita
 %
-rf = Xf(1:3);
-vf = Xf(4:6);
+rf = Xf(1:3); %posicion final
+vf = Xf(4:6); %velocidad final
 %
-vfRel = norm (vf - cross(wV, rf))
+vfRel = norm (vf - cross(wV, rf)) %velociadad relativa final
 %
-eleF = AST_pvecle(Xf,mu);
+eleF = AST_pvecle(Xf,mu); % conversion a elementos orbitales finales
 %
 SAXI_F = eleF(1);
 ECC_F  = eleF(2);
 %
-raf    = SAXI_F*(1 + ECC_F);
-rpf    = SAXI_F*(1 - ECC_F);
+raf    = SAXI_F*(1 + ECC_F); %radio apoapsis final
+rpf    = SAXI_F*(1 - ECC_F); %radio periapsis final
 %
 
-haf = (raf -Re)*1e-3
-hpf = (rpf -Re)*1e-3
+haf = (raf -Re)*1e-3 % comprobacion del error final con respecto al target (apoapsis)
+hpf = (rpf -Re)*1e-3 % comprobacion del error final con respecto al target (periapsis)
